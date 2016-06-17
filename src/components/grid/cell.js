43a61371fb335417efc2f75a91a82605985e1d6b
Eric <ericmckay4@gmail.com>
2016-06-17 10:28:50 -0700

    change border edit boxes

--- src/components/grid/cell.js
+++ src/components/grid/cell.js
@@ -25,48 +25,50 @@ export class Cell extends React.Component {
         };
 
         return (
-            <div
-                className="grid-cell"
-                style={style}
-                onClick={() => { if (mode === 'cell' || mode === 'clear') clickHandler(row, col); }}
-                onMouseEnter={(e) => {
-                    if ((mode === 'cell' || mode === 'clear') && e.buttons === 1) {
-                        clickHandler(row, col);
-                    }
-                }}
-            >
-                {map(content, ((contentObj, key) => (
-                    <div
-                        className={contentClasses(key, activeContentId)}
-                        key={key}
-                        onClick={() => {
-                            if (mode === 'mini-content' || mode === 'main-content') {
-                                contentToggleHandler(row, col, key, contentObj.text);
-                            } else if (mode === 'mini-content-style'
-                                || mode === 'main-content-style') {
-                                clickHandler(row, col, key);
-                            }
-                        }}
-                        style={contentObj.style}
-                    >
-                        {contentObj.text}
-                    </div>
-                )))}
+            <div className="cell-wrapper">
+                <div
+                    className="grid-cell"
+                    style={style}
+                    onClick={() => { if (mode === 'cell' || mode === 'clear') clickHandler(row, col); }}
+                    onMouseEnter={(e) => {
+                        if ((mode === 'cell' || mode === 'clear') && e.buttons === 1) {
+                            clickHandler(row, col);
+                        }
+                    }}
+                >
+                    {map(content, ((contentObj, key) => (
+                        <div
+                            className={contentClasses(key, activeContentId)}
+                            key={key}
+                            onClick={() => {
+                                if (mode === 'mini-content' || mode === 'main-content') {
+                                    contentToggleHandler(row, col, key, contentObj.text);
+                                } else if (mode === 'mini-content-style'
+                                    || mode === 'main-content-style') {
+                                    clickHandler(row, col, key);
+                                }
+                            }}
+                            style={contentObj.style}
+                        >
+                            {contentObj.text}
+                        </div>
+                    )))}
 
-                {[0, 1, 2, 3].map((value) => (
-                    <div
-                        key={value}
-                        className={`border-edit border-edit-${value}`}
-                        onClick={() => {
-                            if (mode === 'single-border') clickHandler(row, col, value);
-                        }}
-                        onMouseEnter={(e) => {
-                            if (mode === 'single-border' && e.buttons === 1) {
-                                clickHandler(row, col, value);
-                            }
-                        }}
-                    />
-                ))}
+                    {[0, 1, 2, 3].map((value) => (
+                        <div
+                            key={value}
+                            className={`border-edit border-edit-${value}`}
+                            onClick={() => {
+                                if (mode === 'single-border') clickHandler(row, col, value);
+                            }}
+                            onMouseEnter={(e) => {
+                                if (mode === 'single-border' && e.buttons === 1) {
+                                    clickHandler(row, col, value);
+                                }
+                            }}
+                        />
+                    ))}
+                </div>
             </div>
         );
     }
